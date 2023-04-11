dc1835888b170f705057b2f715190c37c68d45e9
Alpha Diallo <alphadiallo2235@gmail.com>
2023-04-11 13:47:08 +0000

    Image model created

--- images/models.py
+++ images/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
+class Image(models.Model):
+    user =  models.ForeignKey(settings.AUTH_USER_MODEL,
+                              related_name='images_created',
+                              on_delete=models.CASCADE)
+    title = models.CharField(_('Title'), max_length=200)
+    slug = models.SlugField(_('Slug'), blank=True)
+    url = models.URLField(_('Url'), max_length=2000)
+    image = models.ImageField(_('Image'), upload_to='images/%Y/%m%d')
+    description = models.TextField(_('Description'), blank=True)
+    created = models.DateTimeField(_('Created'), auto_now_add=True)
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created',]
+        
+    def __str__(self) -> str:
+        return self.title
+    
+    def save(self, *args, **kwargs):
+       if not self.slug:
+           self.slug = slugify(self.title)
+       super().save(*args, **kwargs) # Call the real save() method
