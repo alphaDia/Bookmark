88801d91ec92b0d90f015e29e2e76bfa6ed820fe
Alpha Diallo <alphadiallo2235@gmail.com>
2023-04-12 14:38:24 +0000

    Image initial migrations

--- images/models.py
+++ images/models.py
@@ -7,7 +7,12 @@ from django.utils.translation import gettext_lazy as _
 class Image(models.Model):
     user =  models.ForeignKey(settings.AUTH_USER_MODEL,
                               related_name='images_created',
-                              on_delete=models.CASCADE)
+                              on_delete=models.CASCADE
+                            )
+    users_likes = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                         related_name='images_liked',
+                                         blank=True,
+                                        )
     title = models.CharField(_('Title'), max_length=200)
     slug = models.SlugField(_('Slug'), blank=True)
     url = models.URLField(_('Url'), max_length=2000)
